89dc7386835c6a8241df4edc577a6488beca5270
Abdulai059 <abdulaiosman8080@gmail.com>
2025-09-10 17:29:55 +0000

    Small update

--- src/context/postContext.jsx
+++ src/context/postContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useMemo } from "react";
 import { faker } from "@faker-js/faker";
 
 // 1) CREATE CONTEXT
